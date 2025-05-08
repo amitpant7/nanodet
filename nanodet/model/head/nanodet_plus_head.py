d723dfc261b42ee731f655cc6c65a976f94789a9
Amit Pant <50907565+amitpant7@users.noreply.github.com>
2025-05-08 13:38:48 +0545

    Update nanodet_plus_head.py
    Bug fix: The ceil operation was not applied properly and feature map size was float.

--- nanodet/model/head/nanodet_plus_head.py
+++ nanodet/model/head/nanodet_plus_head.py
@@ -477,7 +477,7 @@ class NanoDetPlusHead(nn.Module):
         input_shape = (input_height, input_width)
 
         featmap_sizes = [
-            (math.ceil(input_height / stride), math.ceil(input_width) / stride)
+            (math.ceil(input_height / stride), math.ceil(input_width / stride))
             for stride in self.strides
         ]
         # get grid cells of one image
